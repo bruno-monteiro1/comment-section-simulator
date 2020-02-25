0f0d71755862b0a7534ecf7cebf67d34ba9ff9a7
Bruno Monteiro <brunobapmon@gmail.com>
2020-02-25 19:25:32 -0300

    first commit

---
+++ src/main/java/com/javasb/cssimulator/CssimulatorApplication.java
@@ -0,0 +1,13 @@
+package com.javasb.cssimulator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CssimulatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CssimulatorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/javasb/cssimulator/CssimulatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javasb.cssimulator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CssimulatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
